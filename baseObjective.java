e1409dda023af0c6f07f4961097ec4ec83c0dae3
Eli Brant <eb20id@snpsteam.com>
2024-01-15 10:16:19 -0500

    Added a health bar function and had to add code to the enemy class

---
+++ baseObjective.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class baseObjective here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class baseObjective extends Actor
+{
+    /**
+     * Act - do whatever the baseObjective wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private int health;
+    
+    public baseObjective(){
+        health = 2000;
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
